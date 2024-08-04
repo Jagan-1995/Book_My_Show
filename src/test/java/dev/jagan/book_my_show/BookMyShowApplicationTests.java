96059c8b682f77d62bc2d1dabdecb33587d1619d
Jagan Baskar <jackjagan1995@gmail.com>
2024-08-04 22:38:05 +0530

    Initial commit

---
+++ src/test/java/dev/jagan/book_my_show/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jagan.book_my_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
